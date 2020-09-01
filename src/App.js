72c0c3cd525f679ff068c55774fdd979e8c55fe1
Maciek Lubocki <mail2maciek@gmail.com>
2020-09-01 17:53:21 +0200

    trip filter

--- src/App.js
+++ src/App.js
@@ -1,77 +1,84 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import { connect } from "react-redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {AnimatedSwitch} from 'react-router-transition';
+import styles from './App.scss';
+import PropTypes from "prop-types";
 
-import MainLayout from './components/layout/MainLayout/MainLayout';
+import MainLayout from "./components/layout/MainLayout/MainLayout";
 
-import Home from './components/views/Home/Home';
-import Trips from './components/views/Trips/TripsContainer';
+import Home from "./components/views/Home/Home";
+import Trips from "./components/views/Trips/TripsContainer";
 // TODO - import other views
-import Info from './components/views/Info/Info';
+import Info from "./components/views/Info/Info";
 // import other Countries /ML
-import Countries from './components/views/Countries/CountriesContainer';
+import Countries from "./components/views/Countries/CountriesContainer";
 // import other Country /ML
-import Country from './components/views/Country/CountryContainer';
+import Country from "./components/views/Country/CountryContainer";
 // import other Trip /ML
-import Trip from './components/views/Trip/TripContainer';
+import Trip from "./components/views/Trip/TripContainer";
 // import other Regions /ML
-import Regions from './components/views/Regions/RegionsContainer';
+import Regions from "./components/views/Regions/RegionsContainer";
 
-import NotFound from './components/views/NotFound/NotFound';
+import NotFound from "./components/views/NotFound/NotFound";
 
-import parseTrips from './utils/parseTrips';
-import {setMultipleStates} from './redux/globalRedux';
-
+import parseTrips from "./utils/parseTrips";
+import { setMultipleStates } from "./redux/globalRedux";
 
 class App extends React.Component {
   static propTypes = {
     trips: PropTypes.array,
     setStates: PropTypes.func,
-  }
+  };
 
-  constructor(props){
+  constructor(props) {
     super(props);
     // parse trips when App is first created
     parseTrips(this.props.trips, this.props.setStates);
   }
 
-  componentDidUpdate(prevProps){
-    if(prevProps.trips != this.props.trips){
+  componentDidUpdate(prevProps) {
+    if (prevProps.trips != this.props.trips) {
       // parse trips again if they changed
       parseTrips(this.props.trips, this.props.setStates);
     }
   }
 
-  render(){
+  render() {
     return (
       <BrowserRouter>
         <MainLayout>
-          <Switch location={location}>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/trips' component={Trips} />
+          <AnimatedSwitch
+            atEnter={{ opacity: 0 }}
+            atLeave={{ opacity: 0 }}
+            atActive={{ opacity: 1 }}
+            className={styles.switchAnimation}
+            location={location}
+          >
+            <Route exact path="/" component={Home} />
+            <Route exact path="/trips" component={Trips} />
             {/* TODO - add more routes for other views */}
-            <Route exact path='/info' component={Info} />
+            <Route exact path="/info" component={Info} />
             {/* ML add more routes for other views */}
-            <Route exact path='/countries' component={Countries} />
+            <Route exact path="/countries" component={Countries} />
             {/* ML add more routes for other views */}
-            <Route exact path='/regions' component={Regions} />
-            <Route exact path='/id' component={Country} />
-            <Route exact path='/id' component={Trip} />
-            <Route path='*' component={NotFound} />
-          </Switch>
+            <Route exact path="/regions" component={Regions} />
+            <Route exact path="/country/:id" component={Country} />
+            <Route exact path="/trip/:id" component={Trip} />
+            <Route path="*" component={NotFound} />
+          </AnimatedSwitch>
         </MainLayout>
       </BrowserRouter>
     );
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   trips: state.trips,
 });
 
-const mapDispatchToProps = dispatch => ({
-  setStates: newState => dispatch(setMultipleStates(newState)),
+const mapDispatchToProps = (dispatch) => ({
+  setStates: (newState) => dispatch(setMultipleStates(newState)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -13,26 +13,48 @@ export const getFilteredTrips = ({trips, filters}) => {
 
   // TODO - filter by duration
 
+  if(filters.duration){
+    const pattern = new RegExp(filters.searchPhrase, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+  }
+
+
   // TODO - filter by tags
 
+  if(filters.tags){
+    const pattern = new RegExp(filters.searchPhrase, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+  }
+
+  export const countAllCards = ({cards}) => cards.length.filter(card => new RegExp(searchString, 'i').test(card.title));
+
   // TODO - sort by cost descending (most expensive goes first)
+
+  if(filters.searchPhrase){
+    const pattern = new RegExp(filters.searchPhrase, 'i');
+    output = output.filter(trip => pattern.test(trip.name));
+  }
 
   return output;
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
+  // const filtered = trips;
 
   // TODO - filter trips by tripId
+  
+  const filtered = trips.filter(trip => trip.id == tripId);
 
   console.log('filtering trips by tripId:', tripId, filtered);
   return filtered.length ? filtered[0] : {error: true};
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
+  // const filtered = trips;
 
   // TODO - filter trips by countryCode
+
+  const filtered = trips.filter(trip => country.Code == countryCode);
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
   return filtered.length ? filtered : [{error: true}];
@@ -40,7 +62,7 @@ export const getTripsForCountry = ({trips}, countryCode) => {
 
 /* ACTIONS */
 
-/*
+
 // action name creator
 const reducerName = 'trips';
 const createActionName = name => `app/${reducerName}/${name}`;
@@ -53,4 +75,3 @@ export default function reducer(statePart = [], action = {}) {
       return statePart;
   }
 }
- */
